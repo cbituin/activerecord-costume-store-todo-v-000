f8c99af1d036d7be6e8e9668063fbf4f072c1139
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-05-16 05:10:41 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,10 @@
 # Create your costume_stores migration here
+class CreateCostumeStore < ActiveRecord::Migration[5.1]
+
+  def create
+    create_table :costume_stores do |t|
+        t.string :name
+    end
+  end
+
+end
