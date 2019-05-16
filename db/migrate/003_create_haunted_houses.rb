290c78743d01d63fad7cfcddf6854958ebe4e282
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2019-05-16 05:34:53 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -7,6 +7,8 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.1]
         t.string :name
         t.string :location
         t.string :theme
+        t.integer :price
+        t.boolean :friendly
     end
   end
 
